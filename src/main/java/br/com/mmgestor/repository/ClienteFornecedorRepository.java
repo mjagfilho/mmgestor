c567dc05d19f9961b9507e164509f8c3fc8a589e
Marcilio Gomes Filho <marcilio.filho@gmail.com>
2020-08-28 18:11:25 -0300

    Ajustes nos warnings

--- src/main/java/br/com/mmgestor/repository/ClienteFornecedorRepository.java
+++ src/main/java/br/com/mmgestor/repository/ClienteFornecedorRepository.java
@@ -1,14 +1,13 @@
 package br.com.mmgestor.repository;
 
-import br.com.mmgestor.domain.ClienteFornecedor;
-
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import br.com.mmgestor.domain.ClienteFornecedor;
 
 /**
  * Spring Data  repository for the ClienteFornecedor entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface ClienteFornecedorRepository extends JpaRepository<ClienteFornecedor, Long> {
 }
--- src/main/java/br/com/mmgestor/repository/LocalRepository.java
+++ src/main/java/br/com/mmgestor/repository/LocalRepository.java
@@ -1,14 +1,13 @@
 package br.com.mmgestor.repository;
 
-import br.com.mmgestor.domain.Local;
-
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import br.com.mmgestor.domain.Local;
 
 /**
  * Spring Data  repository for the Local entity.
  */
-@SuppressWarnings("unused")
 @Repository
 public interface LocalRepository extends JpaRepository<Local, Long> {
 }
